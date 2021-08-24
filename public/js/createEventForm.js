d2685194278cab3301d8f2619877086b67c01bc8
acarspl <szczepkowskiadam@wp.pl>
2021-08-24 22:00:10 +0200

    Add (complete) create ride form

--- public/js/createEventForm.js
+++ public/js/createEventForm.js
@@ -1,10 +1,9 @@
 // SPORT SELECTOR
 $(".sport_type_box").on('click',function (e) {
-    $('.sport_type_box').find('img').addClass('typeOfSportImage');
-    $('.sport_type_box').find('h4').addClass('typeOfSportText').removeClass('bg-black');
-    $('#'+this.children[0].id).removeClass('typeOfSportImage');
-    $('#'+this.children[1].id).removeClass('typeOfSportText');
-    $('#'+this.children[1].id).addClass('bg-black');
+    $('.sport_type_box').find('img').addClass('fadedImage');
+    $('.sport_type_box').find('h4').addClass('fadedText').removeClass('bg-green');
+    $('#'+this.children[0].id).removeClass('fadedImage');
+    $('#'+this.children[1].id).removeClass('fadedText').addClass('bg-green');
     switch(this.id){
         case 'road_cycling_box':
             $("#sport_type_id").val(1);
@@ -25,4 +24,144 @@ $(".sport_type_box").on('click',function (e) {
         default:
                 console.log("Error, no type selected!");
     }
+});
+
+// DISTANCE METRIC/IMPERIAL
+let distanceMetric = metricUnits;
+$('#distance_button').on('click',function(){
+    if(distanceMetric){
+        $('#distance_button').addClass('btn-outline-primary').removeClass('btn-primary').text('mi');
+        distanceMetric = false;
+        $('#distance').val(kilometersToMiles($('#distance').val()));
+    }
+    else{
+        $('#distance_button').addClass('btn-primary').removeClass('btn-outline-primary').text('Km');
+        distanceMetric = true;
+        $('#distance').val(milesToKilometers($('#distance').val()));
+    }
+});
+$('#distance').change(function () {
+    setDistanceFinal();
+});
+function  kilometersToMiles(km){
+    return Math.round((km/1.609) * 10) / 10;
+}
+function milesToKilometers(mi){
+    return Math.round((mi*1.609) * 10) / 10;
+}
+function setDistanceFinal(){
+    $('#distance_final').val(convertDistanceToMetrics($('#distance').val()));
+}
+function convertDistanceToMetrics(distance){
+    if(distanceMetric){
+        return distance;
+    }
+    else{
+        return  milesToKilometers(distance);
+    }
+}
+
+// ELEVATION METRIC/IMPERIAL
+let elevationMetric = metricUnits;
+$('#elevation_button').on('click',function(){
+    if(elevationMetric){
+        $('#elevation_button').addClass('btn-outline-primary').removeClass('btn-primary').text('ft');
+        elevationMetric = false;
+        $('#elevation').val(metersToFeet($('#elevation').val()));
+    }
+    else{
+        $('#elevation_button').addClass('btn-primary').removeClass('btn-outline-primary').text('M');
+        elevationMetric = true;
+        $('#elevation').val(feetToMeters($('#elevation').val()));
+    }
+});
+$('#elevation').change(function () {
+    setElevationFinal();
+});
+function  metersToFeet(m){
+   return Math.round(m*3.281);
+}
+function feetToMeters(ft){
+    return Math.round(ft/3.281);
+}
+function setElevationFinal(){
+    $('#elevation_final').val(convertElevationToMetrics($('#elevation').val()));
+}
+function convertElevationToMetrics(elevation){
+    if(elevationMetric){
+        return elevation;
+    }
+    else{
+        return  feetToMeters(elevation);
+    }
+}
+
+// Min Average Speed
+let metricAverageSpeed = metricUnits;
+$('.speed_button').on('click',function(){
+    if(metricAverageSpeed){
+        $('.speed_button').addClass('btn-outline-primary').removeClass('btn-primary').text('mph');
+        metricAverageSpeed = false;
+        $('#speed_min').val(kilometersToMiles($('#speed_min').val()));
+        $('#speed_max').val(kilometersToMiles($('#speed_max').val()));
+    }
+    else{
+        $('.speed_button').addClass('btn-primary').removeClass('btn-outline-primary').text('Kph');
+        metricAverageSpeed = true;
+        $('#speed_min').val(milesToKilometers($('#speed_min').val()));
+        $('#speed_max').val(milesToKilometers($('#speed_max').val()));
+    }
+});
+$('#speed_min').change(function () {
+    setMinSpeedFinal();
+});
+function setMinSpeedFinal(){
+    $('#speed_min_final').val(convertSpeedToMetrics($('#speed_min').val()));
+}
+function convertSpeedToMetrics(speed){
+    if(metricAverageSpeed){
+        return speed;
+    }
+    else{
+        return  milesToKilometers(speed);
+    }
+}
+// Max Average Speed
+$('#speed_max').change(function () {
+    setMaxSpeedFinal();
+});
+function setMaxSpeedFinal(){
+    $('#speed_max_final').val(convertSpeedToMetrics($('#speed_max').val()));
+}
+
+// set signing deadline value and max
+let startTime = null;
+$('#start_time').change(function () {
+    startTime = new Date($('#start_time').val());
+    $('#signing_deadline').val(changeDateToDateTimeLocale(startTime)).attr('max',changeDateToDateTimeLocale(startTime));
+});
+function changeDateToDateTimeLocale(date){
+    let isoStr = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString();
+    return isoStr.substring(0,isoStr.length-1);
+}
+//REQUIREMENTS SELECTOR
+$("#helmet_box").on('click',function (e) {
+    if(Boolean($('#helmet_required').val())===true){
+        $('#helmet_required').val(false);
+    }
+    else{
+        $('#helmet_required').val(true);
+    }
+    $("#helmet_image").toggleClass('strongerFadedImage');
+    $("#helmet_text").toggleClass('fadedText').toggleClass('bg-black-semi-transparent');
+});
+$("#lights_box").on('click',function (e) {
+    if(Boolean($('#lights_required').val())===true){
+        $('#lights_required').val(false);
+    }
+    else{
+        $('#lights_required').val(true);
+    }
+    $("#lights_image").toggleClass('strongerFadedImage');
+    $("#lights_text").toggleClass('fadedText').toggleClass('bg-black-semi-transparent');
 });
--- resources/views/eventManagement/ride/create.blade.php
+++ resources/views/eventManagement/ride/create.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
-<link href="{{ asset('css/createEvent.css') }}" rel="stylesheet">
-@section('content')
 
-    <div class="mt-5 row">
-        <div class="card col-12 col-lg-6 col-xl-5 mx-auto">
+@section('content')
+    <link href="{{ asset('css/createEvent.css') }}" rel="stylesheet">
+    <div class="container mt-5 row mx-auto">
+        <div class="card col-12 col-lg-10 col-xl-9 mx-auto px-0">
             <div class="card-body">
-                <form action="#" method="POST">
+                <form action="{{route('ride.store')}}" method="POST">
                     @csrf
                     @method('post')
                     <div class="form-group">
-                        <label for="startTime">Start Time</label>
-                        <input type="datetime-local" class="form-control" id="startTime" name="start_time" value="{{substr(\Carbon\Carbon::now()->addHour()->toDateTimeLocalString(),0,16)}}"
-                               min="{{substr(\Carbon\Carbon::now()->toDateTimeLocalString(),0,16)}}" max="{{substr(\Carbon\Carbon::now()->addYear()->toDateTimeLocalString(),0,16)}}">
+                        <label for="start_time">Start Time</label>
+                        <input type="datetime-local" class="form-control" id="start_time" name="start_time" value="{{substr(\Carbon\Carbon::now()->addHour()->toDateTimeLocalString(),0,16)}}"
+                               min="{{substr(\Carbon\Carbon::now()->toDateTimeLocalString(),0,16)}}" max="{{substr(\Carbon\Carbon::now()->addYear()->toDateTimeLocalString(),0,16)}}" required>
                     </div>
                     <div class="form-group">
                         <label >Start Location</label>
@@ -33,30 +33,109 @@
                         <input type="hidden" name="sport_type_id" id="sport_type_id" value=0>
                         <div class="row mx-auto justify-content-center">
                             <div class="col-5 mb-1 px-0 bg-black sport_type_box" id="road_cycling_box" style="position: relative; border-top-left-radius:15px;  border-bottom-left-radius:15px">
-                            <img class="typeOfSportImage"  id="road_cycling_image" style="width: 100%; border-top-left-radius:15px;  border-bottom-left-radius:15px" src="{{asset('images/types_of_bikes/road_cycling.jpg')}}" alt="Road Cycling">
-                            <h4 class="overlayTextCenter py-2 text-white text-center col-12 typeOfSportText" id="road_cycling_text">Road Cycling</h4>
+                            <img class="fadedImage"  id="road_cycling_image" style="width: 100%; border-top-left-radius:15px;  border-bottom-left-radius:15px" src="{{asset('images/types_of_bikes/road_cycling.jpg')}}" alt="Road Cycling">
+                            <h4 class="overlayTextCenter py-2 text-white text-center col-12 fadedText" id="road_cycling_text">Road Cycling</h4>
                             </div>
                             <div class="col-5 mx-0 mb-1 px-0 bg-black sport_type_box" id="mtb_box"  style="position: relative; border-top-right-radius:15px;  border-bottom-right-radius:15px">
-                            <img class="typeOfSportImage" id="mtb_image" style="width: 100%; border-top-right-radius:15px; border-bottom-right-radius:15px" src="{{asset('images/types_of_bikes/mtb.jpg')}}" alt="MTB">
-                                <h4 class="overlayTextCenter py-2 text-white text-center col-12 typeOfSportText" id="mtb_text">MTB</h4>
+                            <img class="fadedImage" id="mtb_image" style="width: 100%; border-top-right-radius:15px; border-bottom-right-radius:15px" src="{{asset('images/types_of_bikes/mtb.jpg')}}" alt="MTB">
+                                <h4 class="overlayTextCenter py-2 text-white text-center col-12 fadedText" id="mtb_text">MTB</h4>
                             </div>
                         </div>
                         <div class="row mx-auto justify-content-center">
                             <div class="col-4 mx-0 px-0 bg-black sport_type_box" id="gravel_box"  style="position: relative;  border-top-left-radius:15px; border-bottom-left-radius:15px">
-                                <img class="typeOfSportImage" id="gravel_image" style="width: 100%;  border-top-left-radius:15px; border-bottom-left-radius:15px" src="{{asset('images/types_of_bikes/gravel_ride.jpg')}}" alt="Gravel">
-                                <h4 class="overlayTextCenter py-2 text-white text-center col-12 typeOfSportText" id="gravel_text">Gravel</h4>
+                                <img class="fadedImage" id="gravel_image" style="width: 100%;  border-top-left-radius:15px; border-bottom-left-radius:15px" src="{{asset('images/types_of_bikes/gravel_ride.jpg')}}" alt="Gravel">
+                                <h4 class="overlayTextCenter py-2 text-white text-center col-12 fadedText" id="gravel_text">Gravel</h4>
                             </div>
                             <div class="col-4 mx-0 px-0 bg-black sport_type_box" id="bike_touring_box"  style="position: relative;">
-                                <img class="typeOfSportImage" id="bike_touring_image" style="width: 100%" src="{{asset('images/types_of_bikes/casual_ride_in_park.jpg')}}" alt="Bike Touring">
-                                <h4 class="overlayTextCenter py-2 text-white text-center col-12 typeOfSportText" id="bike_touring_text">Bike Touring</h4>
+                                <img class="fadedImage" id="bike_touring_image" style="width: 100%" src="{{asset('images/types_of_bikes/casual_ride_in_park.jpg')}}" alt="Bike Touring">
+                                <h4 class="overlayTextCenter py-2 text-white text-center col-12 fadedText" id="bike_touring_text">Bike Touring</h4>
                             </div>
                             <div class="col-4 mx-0 px-0 bg-black sport_type_box" id="enduro_box" style="position: relative;  border-top-right-radius:15px; border-bottom-right-radius:15px">
-                                <img class="typeOfSportImage" id="enduro_image" style="width: 100%;  border-top-right-radius:15px; border-bottom-right-radius:15px;" src="{{asset('images/types_of_bikes/enduro.jpg')}}" alt="Enduro">
-                                <h4 class="overlayTextCenter py-2 text-white text-center col-12 typeOfSportText" id="enduro_text">Enduro</h4>
+                                <img class="fadedImage" id="enduro_image" style="width: 100%;  border-top-right-radius:15px; border-bottom-right-radius:15px;" src="{{asset('images/types_of_bikes/enduro.jpg')}}" alt="Enduro">
+                                <h4 class="overlayTextCenter py-2 text-white text-center col-12 fadedText" id="enduro_text">Enduro</h4>
                             </div>
                         </div>
-
-
+                        <div class="form-group mt-4">
+                            <label for="estimated_effort" class="form-label">Estimated Effort</label>
+                            <input type="range" class="form-range form-control" min="1" max="5" step="1" value="3" id="estimated_effort" name="estimated_effort">
+                        </div>
+                        <div class="form-group">
+                            <label for="route_link">Route - Link</label>
+                            <input type="text" class="form-control" id="route_link" name="route_link" placeholder="https://www.strava.com/routes/...">
+                        </div>
+                        <div class="form-group">
+                            <label for="distance">Distance</label>
+                            <div class="row mx-auto">
+                            <input type="number" min="1" max="2000" step="0.1" class="form-control col-10" id="distance" required>
+                                <input type="hidden" min="1"  id="distance_final" name="distance">
+                            <button class="btn btn-primary col-2" type="button" id="distance_button">Km</button>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="elevation">Elevation</label>
+                            <div class="row mx-auto">
+                                <input type="number" min="0" max="30000" step="1" value="0" class="form-control col-10" id="elevation" required>
+                                <input type="hidden" min="0" value="0"  id="elevation_final" name="elevation">
+                                <button class="btn btn-primary col-2" type="button" id="elevation_button">M</button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-6">
+                                <label for="going_outside_website">People already going</label>
+                                <input type="number" min="1" max="500" step="1" value="1" class="form-control" id="going_outside_website" name="going_outside_website" required>
+                            </div>
+                            <div class="form-group col-6">
+                                <label for="max_users">Maximum number of people</label>
+                                <input type="number" min="2" max="2000" step="1" value="100" class="form-control" id="max_users" name="max_users" required>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-6">
+                                <label for="speed_min">Minimum Average Speed</label>
+                                <div class="row mx-auto">
+                                    <input type="number" min="4" max="55" step="0.1" class="form-control col-7 col-lg-10" id="speed_min" required>
+                                    <input type="hidden" min="4"  id="speed_min_final" name="speed_min">
+                                    <button class="btn btn-primary col-5 col-lg-2 speed_button" type="button">Kph</button>
+                                </div>
+                            </div>
+                            <div class="form-group col-6">
+                                <label for="speed_max">Maximum Average Speed</label>
+                                <div class="row  mx-auto">
+                                    <input type="number" min="4" max="55" step="0.1" class="form-control col-7 col-lg-10" id="speed_max" required>
+                                    <input type="hidden" min="4"  id="speed_max_final" name="speed_max">
+                                    <button class="btn btn-primary col-5 col-lg-2 speed_button" type="button">Kph</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="signing_deadline">Signing Deadline</label>
+                            <input type="datetime-local" class="form-control" id="signing_deadline" name="signing_deadline" required
+                            max="{{substr(\Carbon\Carbon::now()->addHour()->toDateTimeLocalString(),0,16)}}" min="{{substr(\Carbon\Carbon::now()->toDateTimeLocalString(),0,16)}}">
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <textarea maxlength="920" class="form-control" id="description" name="description" rows="3"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="additional_information">Additional Information</label>
+                            <textarea maxlength="276" class="form-control" id="additional_information" name="additional_information" rows="1"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label >Requirements</label>
+                            <input type="hidden" id="helmet_required" name="helmet_required" value="true">
+                            <input type="hidden" id="lights_required" name="lights_required" value="false">
+                            <div class="row mx-auto justify-content-center">
+                                <div class="col-5 col-md-4 col-lg-3 mb-1 px-0 bg-black requirements_box" id="helmet_box" style="position: relative; border-top-left-radius:15px;  border-bottom-left-radius:15px">
+                                    <img id="helmet_image" style="width: 100%; border-top-left-radius:15px;  border-bottom-left-radius:15px" src="{{asset('images/equipment/helmet.jpg')}}" alt="Helmet">
+                                    <h4 class="overlayTextRequirement py-1 mb-0 text-white text-center col-12 bg-black-semi-transparent" id="helmet_text" style="border-bottom-left-radius:15px">Helmet</h4>
+                                </div>
+                                <div class="col-5 col-md-4 col-lg-3 mx-0 mb-1 px-0 bg-black requirements_box" id="lights_box"  style="position: relative; border-top-right-radius:15px;  border-bottom-right-radius:15px">
+                                    <img class="strongerFadedImage" id="lights_image" style="width: 100%; border-top-right-radius:15px; border-bottom-right-radius:15px" src="{{asset('images/equipment/lights.jpg')}}" alt="Lights">
+                                    <h4 class="overlayTextRequirement py-1 mb-0 text-white text-center col-12 fadedText" id="lights_text" style="border-bottom-right-radius:15px">Lights</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <button class="btn btn-success btn-block mt-3" type="button">Create</button>
 
                     </div>
                 </form>
@@ -66,6 +145,7 @@
     {{--ADD MAPBOX TOKEN FORM ENV FILE--}}
     <script type="text/javascript">
         let mapBoxToken = "{{config('services.mapbox.token')}}";
+        let metricUnits = true;
     </script>
     <script src="{{ asset('js/createEventMap.js') }}" ></script>
     <script src="{{ asset('js/createEventForm.js') }}" ></script>
