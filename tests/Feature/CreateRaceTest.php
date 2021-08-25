aab5911fcd12ca5db6b05affda238f766ccad1f0
acarspl <szczepkowskiadam@wp.pl>
2021-08-25 22:26:40 +0200

    Create tests for creating race/ride

---
+++ tests/Feature/CreateRaceTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Race;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class CreateRaceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp():void{
+        parent::setUp();
+        $this->seed();
+
+    }
+    public function test_verified_user_can_create_race(){
+        User::factory()->create();
+        $user = Auth::loginUsingId(1);
+        $this->actingAs($user);
+        $race = Race::factory()->make()->toArray();
+        $response = $this->post(route('race.store'),$race);
+        $response->assertStatus(302);
+        $this->assertDatabaseCount('races',1);
+        $this->assertEquals(Race::first()->distance , $race['distance']);
+        $this->assertEquals(Race::first()->user_id , Auth::id());
+    }
+    public function test_guest_cannot_create_race(){
+        $race = Race::factory()->make()->toArray();
+        $response = $this->post(route('race.store'),$race);
+        $response->assertStatus(302)->assertRedirect('/login');
+        $this->assertDatabaseCount('races',0);
+    }
+    public function test_unverified_user_cannot_create_race(){
+        User::factory()->unverified()->create();
+        $user = Auth::loginUsingId(1);
+        $this->actingAs($user);
+        $race = Race::factory()->make()->toArray();
+        $response = $this->post(route('race.store'),$race);
+        $response->assertStatus(302);
+        $this->assertDatabaseCount('races',0);
+    }
+
+}
---
+++ tests/Feature/CreateRideTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Ride;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class CreateRideTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp():void{
+        parent::setUp();
+        $this->seed();
+
+    }
+    public function test_verified_user_can_create_ride(){
+        User::factory()->create();
+        $user = Auth::loginUsingId(1);
+        $this->actingAs($user);
+        $ride = Ride::factory()->make()->toArray();
+        $response = $this->post(route('ride.store'),$ride);
+        $response->assertStatus(302);
+        $this->assertDatabaseCount('rides',1);
+        $this->assertEquals(Ride::first()->distance , $ride['distance']);
+        $this->assertEquals(Ride::first()->user_id , Auth::id());
+    }
+    public function test_guest_cannot_create_ride(){
+        $ride = Ride::factory()->make()->toArray();
+        $response = $this->post(route('ride.store'),$ride);
+        $response->assertStatus(302)->assertRedirect('/login');
+        $this->assertDatabaseCount('rides',0);
+    }
+    public function test_unverified_user_cannot_create_ride(){
+        User::factory()->unverified()->create();
+        $user = Auth::loginUsingId(1);
+        $this->actingAs($user);
+        $ride = Ride::factory()->make()->toArray();
+        $response = $this->post(route('ride.store'),$ride);
+        $response->assertStatus(302);
+        $this->assertDatabaseCount('rides',0);
+    }
+}
