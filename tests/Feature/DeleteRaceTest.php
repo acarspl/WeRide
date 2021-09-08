8feed2a8368b3fd474a59ccae172940f0feb4dd0
acarspl <szczepkowskiadam@wp.pl>
2021-09-08 14:55:29 +0200

    Remove unnecessary dependencies

--- tests/Feature/DeleteRaceTest.php
+++ tests/Feature/DeleteRaceTest.php
@@ -5,9 +5,6 @@ namespace Tests\Feature;
 use App\Models\Race;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
 use Tests\TestCase;
 
 class DeleteRaceTest extends TestCase
